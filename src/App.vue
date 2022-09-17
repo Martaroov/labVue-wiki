e0810beb36c611875f1f47814d2193f4873ecd84
Martaroov <martarodriguezovejero@gmail.com>
2022-09-17 09:31:39 +0200

    wiki

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar />
+  <router-view />
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components: {
+    Navbar,
+  }
+}
+</script>
+
+
+<style>
+html,
+body {
+  font-size: 16px;
+}
+</style>
